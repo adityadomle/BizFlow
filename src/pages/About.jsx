1d7193f55d40d4eb40f88547bad820f73132b27a
Nilam Kumari Mahato <nilamkumarimahato10@gmail.com>
2025-09-09 22:50:55 +0530

    Added hover animation to the boxes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -350,7 +350,7 @@ const AboutUs = () => {
               <motion.div
                 key={index}
                 variants={fadeIn("up", 0.2 * index)}
-                className={`p-6 rounded-xl text-center border border-purple-600 shadow-[0_0_10px_rgba(139,92,246,0.6)]
+                className={`p-6 rounded-xl text-center border border-purple-900 shadow-[0_0_10px_rgba(139,92,246,0.3)] transition-all duration-300 transition- ease-in-out hover:shadow-[0_0_15px_rgba(139,92,246,0.6)] hover:border-purple-600
                 ${
                   isDarkMode
                     ? "bg-white/10 backdrop-blur-md"
@@ -416,7 +416,7 @@ const AboutUs = () => {
               <motion.div
                 key={index}
                 variants={fadeIn("up", 0.2 * index)}
-                className={`p-6 rounded-xl text-center border border-purple-600 shadow-[0_0_10px_rgba(139,92,246,0.6)]
+                className={`p-6 rounded-xl text-center border border-purple-900 shadow-[0_0_10px_rgba(139,92,246,0.2)] transition-all duration-300 transition- ease-in-out hover:shadow-[0_0_15px_rgba(139,92,246,0.6)] hover:border-purple-600
                 ${
                   isDarkMode
                     ? "bg-white/10 backdrop-blur-md"
